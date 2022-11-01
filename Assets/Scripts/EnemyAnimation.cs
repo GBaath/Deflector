da6e44f8228b6ce17e48a312cc9dceedec0fee7e
Lukas Hron <lukasekhr@gmail.com>
2022-11-01 13:26:25 +0100

    Sprites and animation update

---
+++ Assets/Scripts/EnemyAnimation.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimation : MonoBehaviour
+{
+    public Animator enemyAnim;
+
+    public void ShootAnim()
+    {
+        enemyAnim.SetTrigger("Shoot");
+    }
+}
